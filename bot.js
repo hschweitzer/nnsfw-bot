d517c4434b07c9b006853cafd9b3907bd4c8eddd
hschweitzer <hadrian54137@gmail.com>
2019-11-18 16:23:59 +0100

    added winston logging

--- bot.js
+++ bot.js
@@ -1,13 +1,17 @@
 require('dotenv').config()
+const fs = require('fs');
 const Discord = require('discord.js');
 const vision = require('@google-cloud/vision');
 const request = require('request').defaults({ encoding: null });
+const { createLogger, format, transports } = require('winston');
 const client = new vision.ImageAnnotatorClient();
 const bot = new Discord.Client();
 const filename = './servers-settings.json';
+if (!fs.existsSync(filename)) {
+    fs.writeFileSync(filename, '{"servers":{}}');
+}
 const serversettings = require(filename);
 const globalsettings = require('./global-settings.json');
-const fs = require('fs');
 const token = process.env.BOT_TOKEN;
 const ratings = [
     'VERY_UNLIKELY',
@@ -16,6 +20,26 @@ const ratings = [
     'LIKELY',
     'VERY_LIKELY'
 ];
+const logger = createLogger({
+    level: 'info',
+    format: format.combine(
+      format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss'
+      }),
+      format.errors({ stack: true }),
+      format.splat(),
+      format.json()
+    ),
+    defaultMeta: { service: 'nnsfw-bot' },
+    transports: [
+      //
+      // - Write to all logs with level `info` and below to `quick-start-combined.log`.
+      // - Write all logs error (and below) to `quick-start-error.log`.
+      //
+      new transports.File({ filename: './logs/error.log', level: 'error' }),
+      new transports.File({ filename: './logs/combined.log' })
+    ]
+  });
 
 bot.on('message', (msg) => {
     if (!msg.system && !msg.channel.nsfw && !msg.author.bot) {
@@ -85,7 +109,7 @@ bot.on('message', (msg) => {
                         }
                     })
                     .catch(err => {
-                        console.error(err);
+                        logger.error('Google API error: %s', err, new Date());
                     });
                 }
             });
@@ -177,4 +201,5 @@ function getNsfwChannel (serverid, channelid) {
 
 bot.login(token).then(() => {
     console.log('-----------------------------------------------------\n-------------------'+ bot.user.username +' is now Online!--------------\n-----------------------------------------------------');
+    logger.info('%s started running', bot.user.username);
 });
